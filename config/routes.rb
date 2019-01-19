e85912ef44e57ca2677ed94c90530abe0ccfeb7f
Stephanie Olaya <stepholaya@gmail.com>
2019-01-19 17:06:25 -0500

    added new gems and layouts, and updated routes and seeded database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+  resources :articles
+  resources :sessions
+
+  root to: 'articles#index'
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
