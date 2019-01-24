107645e185d5d3fe4abea82b5cccaf619ef3c2fc
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2019-01-23 19:17:04 -0500

    map zooms in now

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,26 +1,29 @@
 class PlacesController < ApplicationController
   def index
-      respond_to do |format|
-        format.html
-        format.json do
-          @places = Place.all
-          render json:  {
-                          type: "FeatureCollection",
-                          features: @places.map do |place|
-                            {
-                              type: "Feature",
-                              geometry: {
-                                type: "Point",
-                                coordinates: [place.longitude, place.latitude]
-                              },
-                              properties: {
-                                name: place.name,
-                                id: place.id
-                              }
+    respond_to do |format|
+      format.html do
+        @coordinates = request.location.coordinates.reverse
+        @coordinates = [0.0, 0.0] if @coordinates.empty?
+      end
+      format.json do
+        @places = Place.all
+        render json:  {
+                        type: "FeatureCollection",
+                        features: @places.map do |place|
+                          {
+                            type: "Feature",
+                            geometry: {
+                              type: "Point",
+                              coordinates: [place.longitude, place.latitude]
+                            },
+                            properties: {
+                              name: place.name,
+                              id: place.id
                             }
-                          end
-                        }
-        end
+                          }
+                        end
+                      }
       end
     end
   end
+end
