b347ae720bd09bac8d16741bc4a0b9ee12192e82
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2019-01-25 13:59:55 -0500

    map finally shows

--- app/javascript/bundles/Map/components/Map.jsx
+++ app/javascript/bundles/Map/components/Map.jsx
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
-import axios from 'axios';
 
 export default class Map extends React.Component {
-  constructor(){
-    super();
-    window.map = this;
-  }
-
   componentDidMount() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYW5keXdlaXNzMTk4MiIsImEiOiJIeHpkYVBrIn0.3N03oecxx5TaQz7YLg2HqA'
-    let { coordinates, centerOnUser } = this.props;
+    let { coordinates } = this.props;
     const mapOptions = {
       container: this.mapContainer,
       style: `mapbox://styles/mapbox/streets-v9`,
@@ -22,85 +16,37 @@ export default class Map extends React.Component {
       maximumAge        : 30000,
       timeout           : 27000
     };
-    if ("geolocation" in navigator && centerOnUser) {
+    if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
         // success callback
         (position) => {
-          coordinates = [
-                          position.coords.longitude,
-                          position.coords.latitude
-                        ];
           document.getElementById("long")
-                  .innerHTML = coordinates[0];
+                  .innerHTML = position.coords.longitude;
           document.getElementById("lat")
-                  .innerHTML = coordinates[1];
-          mapOptions.center = coordinates;
-          this.createMap(mapOptions, geolocationOptions);
+                  .innerHTML = position.coords.latitude;
         },
         // failure callback
-        () => { this.createMap(mapOptions, geolocationOptions) },
+        () => { console.log("Couldn't get location") },
         // options
         geolocationOptions
       );
-    }else{
-      this.createMap(mapOptions, geolocationOptions);
     }
+    this.createMap(mapOptions);
   }
 
-  createMap = (mapOptions, geolocationOptions) => {
+  createMap = mapOptions => {
     this.map = new mapboxgl.Map(mapOptions);
     const map = this.map;
-    const { lat, lng } = map.getCenter();
-    map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: geolocationOptions,
-        trackUserLocation: true
-      })
-    );
     map.on('load', (event) => {
       map.addSource(
         'places',
         {
           type: 'geojson',
-          data: `/places.json?lat=${lat}&lng=${lng}`
+          data: `/places.json`
         }
       );
       map.addLayer({ id: 'places', type: 'circle', source: 'places'});
-      map.on('click', 'places', (e) => {
-        var coordinates = e.features[0].geometry.coordinates.slice();
-        var name = e.features[0].properties.name;
-        var id = e.features[0].properties.id;
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
-        new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML(`<a href="/places/${id}">${name}</a>`)
-            .addTo(map);
-      });
-      map.on('mouseenter', 'places', () => {
-        map.getCanvas().style.cursor = 'pointer';
-      });
-      map.on('mouseleave', 'places', () => {
-        map.getCanvas().style.cursor = '';
-      });
-      map.on('moveend', () => { this.fetchPlaces() });
     });
-  }
-
-  fetchPlaces = () => {
-    const map = this.map;
-    const { lat, lng } = map.getCenter();
-    axios.get(`/places.json?lat=${lat}&lng=${lng}`)
-      .then((response) => { map.getSource('places').setData(response.data) })
-      .catch((error) => { console.log(error) });
-  }
-
-  flyTo = (long, lat) => {
-    const map = this.map;
-    map.flyTo({
-      center: [long, lat]
-    })
   }
 
   render() {
