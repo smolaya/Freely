72ed655fe738589a1dbad05e36cfd445212322c7
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2019-01-25 13:59:03 -0500

    fixed bug and restarted chrome

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,12 @@
 class PlacesController < ApplicationController
-
-  def create
-    @place = Place.new(place_params)
-    if @place.save
-      render :create
-    else
-      redirect_to places_path, notice: @place.errors.join(', ')
-    end
-  end
-
   def index
     respond_to do |format|
       format.html do
         @coordinates = request.location.coordinates.reverse
         @coordinates = [0.0, 0.0] if @coordinates.empty?
-        @place = Place.new
       end
       format.json do
-        @places = Place.near([params[:lat], params[:lng]], 50)
+        @places = Place.all
         render json:  {
                         type: "FeatureCollection",
                         features: @places.map do |place|
@@ -37,15 +26,4 @@ class PlacesController < ApplicationController
       end
     end
   end
-
-  def show
-    @place = Place.find(params[:id])
-  end
-
-  private
-
-  def place_params
-    params.require(:place).permit(:name, :street, :city, :state, :country)
-  end
-
 end
