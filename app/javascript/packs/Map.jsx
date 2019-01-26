c429866ae6afa85381473f3754823c55b646a60d
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2019-01-26 15:41:05 -0500

    hide info in Map.jsx

--- app/javascript/packs/Map.jsx
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import Map from '../bundles/Map/components/Map';
-
-// This is how react_on_rails can see the Map in the browser.
-ReactOnRails.register({
-  Map,
-});
