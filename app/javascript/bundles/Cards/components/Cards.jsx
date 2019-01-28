bc6aed34b6380bf1759c9ce616b5e8e31003cd7a
Robert Beavin <rmbeavin@gmail.com>
2019-01-28 16:04:42 -0500

    created card component for use with carousel

--- app/javascript/bundles/Cards/components/Cards.jsx
+++ app/javascript/bundles/Cards/components/Cards.jsx
@@ -1,39 +1,25 @@
-import React from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
 import { Grid, Card } from 'semantic-ui-react';
+import CardsList from '../bundles/Cards/components/CardsList';
 
-export default class Cards extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      cards: []
-    }
-  }
-
-  onSubmit = (e) => {
-    e.preventDefault();
-    axios.get(`https://www.eventbriteapi.com/v3/events/search/?location.latitude=25.8014116&location.longitude=-80.1990871&location.within=25km&expand=venue,category&token=N3UJC5A67XVRFFOQQBCG`).then(({data}) => {
-      this.setState({cards: data})
-      console.log({data})
-    }).catch(err => console.log(err))
-  }
-
-  makeCards = () => {
-    this.state.map((cards, index) => {
-      return 
-    })
-  }  
-
-  render() {
-    return (
-     
-      <div>
-        <p>Map coordinates</p>
-        <button className="submitbutton" onClick={this.onSubmit}>Submit</button>
-        <button className="submitbutton" onClick={this.makeCards}>MakeCards</button>
-        <p>{this.state.cards["name"]}</p>
-      </div>
-      
-    )
-  }
-}
+const Cards = props => {
+  return (
+    <div>
+      <Grid.Column>
+        <Grid.Row>
+          <Card>
+            <Card.Content>
+              <Card.Header>Card</Card.Header>
+              <Card.Meta>
+                <span>{this.props.name}</span><br/>
+              </Card.Meta>
+              <Card.Description>{this.props.cards}</Card.Description>
+            </Card.Content>
+          </Card>
+        </Grid.Row>
+      </Grid.Column>
+    </div>   
+  )
+}  
+export default Cards
