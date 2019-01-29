016143a5c2d812043f682081d7ba1a91a09fa577
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2019-01-28 19:51:04 -0500

    added routes componentDidMount

--- app/javascript/bundles/Cards/components/CardsList.jsx
+++ app/javascript/bundles/Cards/components/CardsList.jsx
@@ -3,18 +3,43 @@ import axios from 'axios';
 import { Grid, Card } from 'semantic-ui-react';
 import Cards from './Cards';
 
+const token = document
+              .querySelector('meta[name="csrf-token"]')
+              .getAttribute('content')
+
+const headers = {
+  'X-Requested-With': 'XMLHttpRequest',
+  'X-CSRF-TOKEN': token
+  }
+
 export default class CardsList extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      cards: []
+      cards: [],
+      events: [],
     }
   }
 
-  componentDidMount() {
-    axios.get(`https://www.eventbriteapi.com/v3/events/search/?location.latitude=25.8014116&location.longitude=-80.1990871&location.within=25km&expand=venue,category&token=N3UJC5A67XVRFFOQQBCG`).then(({data}) => {
-      this.setState({cards: data.events})
-    }).catch(err => console.log(err))
+  async componentDidMount() {
+    let { events } = this.state
+    const { data } = await axios.get(`https://www.eventbriteapi.com/v3/events/search/?location.latitude=25.8014116&location.longitude=-80.1990871&location.within=25km&expand=venue,category&token=N3UJC5A67XVRFFOQQBCG`)
+    events = data.events.map(event => ({
+      name: event.name.text,
+      street: event.venue.address.address_1,
+      city: event.venue.address.city,
+      state: event.venue.address.region,
+      longitude: event.venue.longitude,
+      latitude: event.venue.latitude
+    }))
+    events.forEach(async (event) => {
+      await axios.post(
+        '/places',
+        event,
+        headers: headers,
+      )
+    })
+    this.setState({ cards: data.events, events })
   }
 
   render() {
@@ -30,4 +55,4 @@ export default class CardsList extends React.Component {
         </div>
     )
   }
-} 
+}
