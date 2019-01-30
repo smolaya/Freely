bd154cada976315202659debde3a90ef1ee0b731
Robert Beavin <rmbeavin@gmail.com>
2019-01-30 14:56:47 -0500

    added button to cards to submit post request to rails db to add event to db

--- app/javascript/bundles/Cards/components/Cards.jsx
+++ app/javascript/bundles/Cards/components/Cards.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import { Grid, Card, Button } from 'semantic-ui-react';
-import CardsList from './Cards';
+import csrfHeaders from '../../shared/csrfHeaders.jsx'
 import axios from 'axios';
 
 class Cards extends React.Component {
 
   addEvent = e => {
-    let data =  { 
-      events:{
-        name: this.props.eventcard.name.text, 
-        address: this.props.eventcard.venue.address.localized_address_display, 
-        datetime_start: this.props.eventcard.start.local, 
-        datetime_end: this.props.eventcard.end.local, 
-        description: this.props.eventcard.description.text, 
-        category: this.props.eventcard.category.name
-      }
+    let event = {
+      name: this.props.eventcard.name.text, 
+      address: this.props.eventcard.venue.address.localized_address_display, 
+      datetime_start: this.props.eventcard.start.local, 
+      datetime_end: this.props.eventcard.end.local, 
+      description: this.props.eventcard.description.text, 
+      category: this.props.eventcard.category.name
     }
 
-    $.ajax({
-      method: 'POST',
-      data: data,
-      headers: {
-       'Content-Type': 'application/json'
-      },
-      url: 'http://localhost:3000/api/v1/events'
-    })
-      .done( data => console.log( data ) )
-      .fail( err => console.log( err ) )
-      .always( console.log( "done w ajax" ) )
+    let formData = new FormData()
+    for (let item in event){
+      formData.append( `event[${item}]`, event[item]  )
+    }
+
+    // $.ajax({
+    //   method: 'POST',
+    //   data: formData,
+    //   headers: {
+    //    'Content-Type': 'application/json'
+    //   },
+    //   url: '/api/v1/events'
+    // })
+    axios.post('/api/v1/events.json', formData, { headers: csrfHeaders } )
+      .then( data => console.log( data ) )
+      .catch( err => console.log( err ) )
   }
 
     render(){
