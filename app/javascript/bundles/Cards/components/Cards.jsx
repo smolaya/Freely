af2f11b0d6f87e4fafeb79b8c78d554fa1dd847a
Ana Gonzalez <ana.m.gonzalez0496@gmail.com>
2019-01-31 20:09:27 -0500

    start of the carousel

--- app/javascript/bundles/Cards/components/Cards.jsx
+++ app/javascript/bundles/Cards/components/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid, Card, Button } from 'semantic-ui-react';
 import csrfHeaders from '../../shared/csrfHeaders.jsx'
 import axios from 'axios';
+import Carousel from 'nuka-carousel'
 
 class Cards extends React.Component {
 
@@ -35,11 +36,8 @@ class Cards extends React.Component {
 
     render(){
       return (
-        <div>
-          <Grid.Column>
-            <Grid.Row>
-            <Card>
-                <Card.Content>
+        <Carousel slidesToShow={3} cellAlign="center">
+              <Card>
                   <Card.Header>{this.props.eventcard.name.text}</Card.Header>
                   <Card.Meta>
                     Start time: {this.props.eventcard.start.local} <br />
@@ -47,13 +45,10 @@ class Cards extends React.Component {
                     Address: {this.props.eventcard.venue.address.localized_address_display}<br />
                   </Card.Meta>
                   <Card.Description>{this.props.eventcard.description.text}</Card.Description>
-                </Card.Content>
                 <Button onClick={this.addEvent}>Add to My Calendar</Button>
               </Card>
-            </Grid.Row>
-          </Grid.Column>
-        </div>
-      )
+        </Carousel>
+      );
     }
 }
 export default Cards
