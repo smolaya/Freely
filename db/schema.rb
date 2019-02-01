9dc043cd7ef59f923872e820f75f03b2448bce2e
Robert Beavin <rmbeavin@gmail.com>
2019-02-01 11:16:59 -0500

    ran db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_205319) do
+ActiveRecord::Schema.define(version: 2019_01_31_235531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,10 @@ ActiveRecord::Schema.define(version: 2019_01_30_205319) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_events_on_user_id"
+    t.index ["users_id"], name: "index_events_on_users_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -67,4 +69,5 @@ ActiveRecord::Schema.define(version: 2019_01_30_205319) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "events", "users", column: "users_id"
 end
