e9292be3752060eab60981bd760aa93f92e10dad
Robert Beavin <rmbeavin@gmail.com>
2019-02-01 19:45:22 -0500

    updated calendar controler

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -5,13 +5,10 @@ class CalendarController < ApplicationController
       format.json do
         start_date  = Date.parse(params[:start_date])
         end_date    = Date.parse(params[:end_date])
-        events  = current_user.events
-                    .ordered
-                    .between(start_date, end_date)
-                    .group_by{|e| e.datetime_start.to_date }
-    
+        events  = current_user.events.ordered.between(start_date, end_date).group_by{|e| e.datetime_start.strftime("%Y-%m-%d") }
         render json: events
       end
     end
   end
 end
+
